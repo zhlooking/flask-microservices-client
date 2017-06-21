c12c267ad3d37efd8e33f239a529c5bb36b1babb
zhlookin <zhanghao@guangzixuexi.com>
2017-06-22 00:18:53 +0800

    modify component

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ class App extends React.Component {
         super();
         this.state = {
             users: [],
+            username: '',
+            email: '',
         }
     }
 
@@ -24,11 +26,68 @@ class App extends React.Component {
             })
     }
 
+    handleChange(evt) {
+        if (evt.target.name === 'username') {
+            this.setState({ username: evt.target.value })
+        } else {
+            this.setState({ email: evt.target.value })
+        }
+    }
+
+    handleSubmit(evt) {
+        evt.preventDefault();
+        const data = {
+            username: this.state.username,
+            email: this.state.email,
+        };
+        axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+            .then(() => {
+                this.getUsers();
+                this.setState({ username: '', email: '' });
+            })
+            .catch((error) => { console.log(error)})
+    }
+
+    _renderAddUser() {
+        return (
+            <form onSubmit={this.handleSubmit.bind(this)}>
+                <div className="form-group">
+                    <input
+                        name="username"
+                        className="form-control input-lg"
+                        type="text"
+                        placeholder="Enter a username"
+                        required
+                        value={this.state.username}
+                        onChange={this.handleChange.bind(this)}
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        name="email"
+                        className="form-control input-lg"
+                        type="email"
+                        placeholder="Enter an email address"
+                        required
+                        value={this.state.email}
+                        onChange={this.handleChange.bind(this)}
+                    />
+                </div>
+                <input
+                    type="submit"
+                    className="btn btn-primary btn-lg btn-block"
+                    value="Submit"
+                />
+            </form>
+        )
+    }
+
     render() {
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-md-4">
+                        {this._renderAddUser()}
                         <br/>
                         <h1>All Users</h1>
                         <hr/><br/>
@@ -39,6 +98,7 @@ class App extends React.Component {
         )
     }
 }
+
 
 ReactDOM.render(
   <App />,
